d1d1080e33ff4ce241191d92935f15d434755a9a
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-07-26 21:29:09 +0545

    heelo

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ let PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 app.use(cors({ 
-  origin: ["https://ecommerce-mern-theta-kohl.vercel.app"],
+  origin: ["https://ecommerce-mern-theta-kohl.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
